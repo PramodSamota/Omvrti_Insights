d46a606f75674a2c2524252f12a1761062a498ce
Pramod Samota <pramodsamota21@gmail.com>
2026-02-20 15:39:20 +0530

    Fix allowed host for Render

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+       server: {
+    allowedHosts: [
+      'omvrti-insights.onrender.com'
+    ]
+  }
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
@@ -21,3 +26,4 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
