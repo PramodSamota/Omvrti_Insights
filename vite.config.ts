c713368efe8031335cf59749be82a0d01ca721dd
Pramod Samota <pramodsamota21@gmail.com>
2026-02-20 15:42:16 +0530

    change viteCongig for allow Headers

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,13 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-      },
-      plugins: [react()],
-       server: {
-    allowedHosts: [
+        allowedHosts: [
       'omvrti-insights.onrender.com'
     ]
-  }
+      },
+      plugins: [react()],
+     
+  
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
